326569a3bf7647d4992d0c1ab48ef277c498a144
Jithu <jithukrishnaju@gmail.com>
2023-06-21 14:52:32 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
+import {
+  TextField,
+  Typography,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
+import { useState } from "react";
 function App() {
+  const [textvalues, changetextvalues] = useState("");
+  const [todos, changetodos] = useState([]);
+  const submitbtn = () => {
+    changetodos([...todos, textvalues]);
+    changetextvalues("");
+  };
+  const handleDelete = (index) => {
+    changetodos(todos.filter((todos, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todobox">
+      <Box
+        sx={{
+          boxShadow: 12,
+          margin: 2,
+          padding: 2,
+        }}
+      >
+        <Box>
+          <Typography sx={{ textAlign: "center" }}>REMINDER APP</Typography>
+
+          <TextField
+            onChange={(e) => changetextvalues(e.target.value)}
+            value={textvalues}
+            sx={{
+              width: "350px",
+              marginRight: "8px",
+              marginTop: "10px",
+            }}
+            label="Enter a reminder"
+            variant="outlined"
+            size="small"
+          />
+          <Button
+            onClick={submitbtn}
+            sx={{
+              width: "170px",
+              marginTop: "10px",
+            }}
+            variant="contained"
+          >
+            Add a reminder
+          </Button>
+        </Box>
+        <Box sx={{ width: "300px" }}>
+          <List
+            style={{ flexWrap: "wrap" }}
+            sx={{
+              justifyContent: "center",
+              listStyle: "none",
+              gap: "10px",
+            }}
+          >
+            {todos.map((text, index) => (
+              <ListItem key={index}>
+                <div className="todostyles">
+                  <ListItemText
+                    sx={{ height: "20px" }}
+                    primary={text.toUpperCase()}
+                    className="todostyles"
+                  />
+                  <Button
+                    onClick={() => handleDelete(index)}
+                    sx={{ width: "30px", height: "40px" }}
+                    variant="contained"
+                    color="error"
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
     </div>
   );
 }
